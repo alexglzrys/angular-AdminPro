852db68d7e99e8e9b69cb807a7b638dec1dbca09
Alejandro González Reyes <alexglzrys@gmail.com>
2021-12-29 18:48:55 -0600

    diseño página 404

--- src/app/pages/no-page-found/no-page-found.component.ts
+++ src/app/pages/no-page-found/no-page-found.component.ts
@@ -3,10 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-no-page-found',
   templateUrl: './no-page-found.component.html',
-  styles: [
-  ]
+  styleUrls: ['./no-page-found.component.css'],
 })
 export class NoPageFoundComponent implements OnInit {
+
+  year: number = new Date().getFullYear();
 
   constructor() { }
 
