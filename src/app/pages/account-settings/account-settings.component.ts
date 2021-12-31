3263034e2003e98f707a8e9cc9c23fc70b5f8436
Alejandro González Reyes <alexglzrys@gmail.com>
2021-12-31 12:31:33 -0600

    cambiar tema CSS de forma dinámica

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -8,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSettingsComponent implements OnInit {
 
+  // Obtener la referencia al elemento de link CSS por su id (ver index.html)
+  elementLink: HTMLElement | null = document.querySelector('#theme');
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // Cambiar el tema de nuestra aplicación (este es un archivo CSS con el nombre del tema pasado como argumento)
+  changeTheme(theme: string) {
+
+    const elementHref = `./assets/css/colors/${theme}.css`;
+    // Establecer nuevo archivo de CSS al elemento
+    this.elementLink?.setAttribute('href', elementHref);
+
+    // Guardar el LocalStorage para persistir los cambios de tema seleccionado
+    localStorage.setItem('theme', elementHref);
   }
 
 }
