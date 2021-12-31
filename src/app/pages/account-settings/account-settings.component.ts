9e0521f609341189e8d79c4431da77d01e07fbf7
Alejandro González Reyes <alexglzrys@gmail.com>
2021-12-31 13:19:57 -0600

    agregar clase checked al elemento del tema actualmente seleccionado

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -10,10 +10,17 @@ export class AccountSettingsComponent implements OnInit {
 
   // Obtener la referencia al elemento de link CSS por su id (ver index.html)
   elementLink: HTMLElement | null = document.querySelector('#theme');
+  links!:NodeListOf<HTMLElement>;
 
   constructor() { }
 
   ngOnInit(): void {
+    // En este punto el HTML del componente ya existe
+    // Recuperar el listado de elementos (enlaces) que hacen referencia a los temas disponibles
+    this.links = document.querySelectorAll('.selector');
+
+    // Establecer el checked que se corresponde con el tema actualmente selecciondo
+    this.checkCurrentTheme()
   }
 
   // Cambiar el tema de nuestra aplicación (este es un archivo CSS con el nombre del tema pasado como argumento)
@@ -22,9 +29,27 @@ export class AccountSettingsComponent implements OnInit {
     const elementHref = `./assets/css/colors/${theme}.css`;
     // Establecer nuevo archivo de CSS al elemento
     this.elementLink?.setAttribute('href', elementHref);
-
     // Guardar el LocalStorage para persistir los cambios de tema seleccionado
     localStorage.setItem('theme', elementHref);
+
+    // Colocar el checked al elemento actualmente seleccionado
+    this.checkCurrentTheme()
+  }
+
+  // Colocar un checked al tema seleccionado
+  checkCurrentTheme() {
+    // Iterar por cada elemento de enlace, retirar la clase working,
+    // y colocarla solo en el enlace que haga referencia al tema actualmente seleccionado
+    // la propiedad data-theme contiene el nombre del tema que activa ese elemento
+    this.links.forEach(element => {
+      element.classList.remove('working');
+      const url = `./assets/css/colors/${element.dataset.theme}.css`;
+      const currentCSS = this.elementLink?.getAttribute('href');
+
+      if (url === currentCSS) {
+        element.classList.add('working');
+      }
+    })
   }
 
 }
