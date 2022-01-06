cd191a08733039f18cfad164d9b8593e947e7a54
Alejandro González Reyes <alexglzrys@gmail.com>
2022-01-06 14:58:52 -0600

    guardar información en localStorage

--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { LoginForm } from '../interfaces/login-form';
 import { RegisterUserForm } from '../interfaces/register-user-form';
@@ -16,12 +17,26 @@ export class UsuariosService {
 
   registrarUsuario(formData: RegisterUserForm): Observable<any> {
     const URL = `${ BASE_URL }/usuarios`;
-    return this.http.post(URL, formData);
+    return this.http.post(URL, formData).pipe(
+      tap((res: any) => {
+        // Efecto secundario, necesito guardar el token en LocalStorage si el registro es correcto
+        // Si no viene la propiedad token, no se crea en localStorage
+        localStorage.setItem('token', res.token);
+      })
+      // Continuar con el flujo;
+    );
   }
 
   login(formData: LoginForm): Observable<any> {
     const URL = `${ BASE_URL }/login`;
-    return this.http.post(URL, formData);
+    return this.http.post(URL, formData).pipe(
+      tap((res: any) => {
+        // Efecto secundario, necesito guardar el token en LocalStorage si el login es correcto
+        // Si no viene la propiedad token, no se crea en localStorage
+        localStorage.setItem('token', res.token);
+      })
+      // Continuar con el flujo
+    );
   }
 
 }
