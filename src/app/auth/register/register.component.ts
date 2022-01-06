4df954e849bd62de174fabb5f1af692805e06c5a
Alejandro González Reyes <alexglzrys@gmail.com>
2022-01-06 13:41:57 -0600

    validación contraseñas iguales

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -15,6 +15,9 @@ export class RegisterComponent implements OnInit {
     password: ['', [Validators.required, Validators.minLength(8)]],
     password_confirm: ['', [Validators.required, Validators.minLength(8)]],
     terminos: [false, Validators.required]
+  }, {
+    // Validators personalizados sincronos
+    validators: this.passwordIguales('password', 'password_confirm')
   });
 
   formSubmitted: boolean = false;
@@ -36,6 +39,8 @@ export class RegisterComponent implements OnInit {
     }
   }
 
+  // Funciones para mostrar mensajes de validación
+
   campoNoValido(campo: string): boolean {
     if (this.registerForm.get(campo)?.invalid && this.formSubmitted) {
       return true;
@@ -44,8 +49,29 @@ export class RegisterComponent implements OnInit {
     }
   }
 
+  passwordsNoIguales(): boolean {
+    return (this.registerForm.get('password')?.value !== this.registerForm.get('password_confirm')?.value) && this.formSubmitted;
+  }
+
   noAceptaTerminos(): boolean {
     return !this.registerForm.get('terminos')?.value && this.formSubmitted;
+  }
+
+  // Validator personalizado
+
+  passwordIguales(campo1: string, campo2: string): Function {
+    // Retornar una función, pues recibe parámetros
+    return (formGroup: FormGroup) => {
+      const pass1 = formGroup.get('password');
+      const pass2 = formGroup.get('password_confirm');
+
+      if (pass1?.value === pass2?.value) {
+        pass2?.setErrors(null);
+      } else {
+        // Hay errores y debo retornar el objeto con el error
+        pass2?.setErrors({ noIguales: true });
+      }
+    }
   }
 
 }
