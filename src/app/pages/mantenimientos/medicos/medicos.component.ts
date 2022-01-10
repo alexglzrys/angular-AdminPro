c5e26268db4ccb0222e2ac1fda6418f581a425db
Alejandro González Reyes <alexglzrys@gmail.com>
2022-01-10 17:55:13 -0600

    agregar servicio de médicos

--- src/app/pages/mantenimientos/medicos/medicos.component.ts
+++ src/app/pages/mantenimientos/medicos/medicos.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Medico } from 'src/app/models/medico.model';
+import { MedicoService } from 'src/app/services/medico.service';
 
 @Component({
   selector: 'app-medicos',
@@ -6,11 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class MedicosComponent implements OnInit {
+export class MedicosComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  medicos: Medico[] = [];
+  loader: boolean = false;
+
+  constructor(private medicoService: MedicoService) { }
+
+  ngOnDestroy(): void {
+
+  }
 
   ngOnInit(): void {
+    this.cargarMedicos();
+  }
+
+  cargarMedicos() {
+    this.loader = true;
+    this.medicoService.getMedicos().subscribe(medicos => {
+      this.medicos = medicos;
+    },
+    err => {
+
+    },
+    () => {
+      this.loader = false;
+    })
   }
 
 }
--- src/app/services/medico.service.ts
+++ src/app/services/medico.service.ts
@@ -1,6 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { delay, map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { Medico } from '../models/medico.model';
 
+const BASE_URL = environment.base_url;
 @Injectable({
   providedIn: 'root'
 })
@@ -8,5 +13,44 @@ export class MedicoService {
 
   constructor(private http: HttpClient) { }
 
+  get token(): string {
+    return localStorage.getItem('token') || '';
+  }
+
+  get headers(): object {
+    return {
+      headers: {
+        'x-token': this.token
+      }
+    }
+  }
+
+  getMedicos(): Observable<Medico[]> {
+    const URL = `${BASE_URL}/medicos`;
+    return this.http.get<{ ok: boolean, medicos: Medico[] }>(URL, this.headers).pipe(
+      delay(1000),
+      map(res => {
+        // Solo me interesa devolver el listado de médicos
+        return res.medicos;
+      })
+    );
+  }
+
+  // Se puede mandar X información en el cuerpo de la petición.
+  // Sin embargo, el backend solo tomará en cuenta la información que necesita
+  registrarMedico(medico: Medico): Observable<any> {
+    const URL = `${BASE_URL}/medicos`;
+    return this.http.post(URL, medico, this.headers);
+  }
+
+  actualizarMedico(medico: Medico): Observable<any> {
+    const URL = `${BASE_URL}/medicos/${medico._id}`;
+    return this.http.put(URL, medico, this.headers );
+  }
+
+  eliminarMedico(medico: Medico): Observable<any> {
+    const URL = `${BASE_URL}/medicos/${medico._id}`;
+    return this.http.delete(URL, this.headers);
+  }
 
 }
